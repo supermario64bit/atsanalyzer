d649e67bccdf1cd7de90916fbfe6896322e3a2f8
Mr. Mario <gokulsujan.dev@gmail.com>
2025-10-17 10:47:23 +0530

    Optimised prompt

--- dto/ats_analysis.go
+++ dto/ats_analysis.go
@@ -1,8 +1,13 @@
 package dto
 
 type ResumeAnalysis struct {
-    AtsScore            int      `json:"ats_match_score"`
-    MatchingSkills      []string `json:"matched_skills"`
-    MissingSkills       []string `json:"missing_skills"`
-    SuggestionsToImprove []string `json:"suggestions_to_improve"`
+	AtsScore             int      `json:"ats_match_score"`
+	MatchingSkills       []string `json:"matched_skills"`
+	MissingSkills        []string `json:"missing_skills"`
+	SuggestionsToImprove []string `json:"suggestions_to_improve"`
+	CandiateName         string   `json:"candidate_name"`
+	CandidateEmail       string   `json:"candidate_email"`
+	CandidatePhone       string   `json:"candidate_phone"`
+	CompanyName          string   `json:"company_name"`
+	JobRole              string   `json:"role"`
 }
--- utils/analysis_schema.go
+++ utils/analysis_schema.go
@@ -8,7 +8,7 @@ func GenerateAnalysisSchema() *genai.Schema {
 		Properties: map[string]*genai.Schema{
 			"ats_match_score": {
 				Type:        genai.TypeInteger,
-				Description: "ATS match percentage (0-100)",
+				Description: "ATS match percentage (0-100). Match criteria will be the required skills, experience and background in the job description matches with the candidate resume",
 			},
 			"matched_skills": {
 				Type:        genai.TypeArray,
@@ -24,6 +24,26 @@ func GenerateAnalysisSchema() *genai.Schema {
 				Type:        genai.TypeArray,
 				Description: "Specific suggestions to enhance the resume for a better match with the job description in simple human readable english.",
 				Items:       &genai.Schema{Type: genai.TypeString},
+			},
+			"candidate_name": {
+				Type:        genai.TypeString,
+				Description: "Candidate name given in the resume. If name not available show, 'Unable to extract candidate name from resume'",
+			},
+			"candidate_email": {
+				Type:        genai.TypeString,
+				Description: "Candidate email given in the resume. If email not available show, 'Unable to extract candidate email from resume.'",
+			},
+			"candidate_phone": {
+				Type:        genai.TypeString,
+				Description: "Candidate phone number given in the resume. If number not available show, 'Unable to extract candidate phone from resume.'",
+			},
+			"company_name": {
+				Type:        genai.TypeString,
+				Description: "Company name given in the job description. If not available show, 'Unable to extract company name from JD.'",
+			},
+			"role": {
+				Type:        genai.TypeString,
+				Description: "Job Role given in the job description that the company is hiring. If not available show, 'Unable to extract job role from JD.'",
 			},
 		},
 		Required: []string{"ats_match_score", "matched_skills", "missing_skills", "suggestions_to_improve"},
